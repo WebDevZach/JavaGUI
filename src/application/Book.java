d4bd58e2b6332579d3401b4c5fe48b882cad79e0
Zachary Leyes <leyeszach@gmail.com>
2023-07-30 18:47:15 -0400

    encapsulated member fields for the book class

--- src/application/Book.java
+++ src/application/Book.java
@@ -5,65 +5,54 @@ import java.util.Date;
 
 public class Book {
 
-	 String title;
-	 String author;
-	 String status;
-	 Date lentDate;
-	 int id;
-	
-	
-	//Constructor for adding a new book
+	private String title;
+	private String author;
+	private String status;
+	private Date lentDate;
+	private int id;
+
+	// Constructor for adding a new book
 	Book(String title, String author) {
 		this.title = title;
 		this.author = author;
 		this.status = "available";
 		this.lentDate = null;
 	}
-	
-	public Date getDate()
-	{
+
+	public Date getDate() {
 		return lentDate;
 	}
-	
-	public String getTitle()
-	{
+
+	public String getTitle() {
 		return title;
 	}
-	
-	public String getAuthor()
-	{
+
+	public String getAuthor() {
 		return author;
 	}
-	
-	public String getStatus()
-	{
+
+	public String getStatus() {
 		return status;
 	}
-	
-	public int getId()
-	{
+
+	public int getId() {
 		return id;
 	}
 
-	public void rentBook()
-	{
+	public void rentBook() {
 		this.status = "unavailable";
 		this.lentDate = new Date();
 	}
-	
 
-	public void returnBook()
-	{
+	public void returnBook() {
 		this.status = "available";
 		this.lentDate = null;
 	}
-	
-	public void setId(ArrayList<Book> bookCatalog)
-	{
+
+	public void setId(ArrayList<Book> bookCatalog) {
 		int numOfBooks = bookCatalog.size();
 		int id = numOfBooks + 1;
 		this.id = id;
 	}
-	
-	
+
 }
