05e64da1804f99dd9a017b6d7f5035461f387566
Zachary Leyes <leyeszach@gmail.com>
2023-07-30 13:42:01 -0400

    refactor code added comments as well

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.util.Scanner;
-
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -16,111 +14,133 @@ import javafx.scene.layout.HBox;
 import java.util.ArrayList;
 
 public class Main extends Application {
-	
-	
+
 	private TextField bookTitleTf;
 	private TextField authorTf;
 
-
-
 	@Override
 	public void start(Stage primaryStage) {
-		
-		
+
 		ArrayList<Book> bookCatalog = new ArrayList<>();
 
-		VBox layout1 = new VBox();
-		layout1.setSpacing(35);
+		
+		
+		/* MENU SECTION */
+
+		VBox menuLayout = new VBox();
+		menuLayout.setSpacing(35);
 		BorderPane borderPane = new BorderPane();
-		borderPane.setTop(layout1);
-		layout1.setPadding(new Insets(30, 0, 0, 150));
+		borderPane.setTop(menuLayout);
+		menuLayout.setPadding(new Insets(30, 0, 0, 150));
 		Scene scene1 = new Scene(borderPane, 400, 300);
 
-		VBox layout3 = new VBox();
-		VBox layout4 = new VBox();
-		VBox layout5 = new VBox();
-
-		
-		Scene scene3 = new Scene(layout3, 400, 300);
-		Scene scene4 = new Scene(layout4, 400, 300);
-		Scene scene5 = new Scene(layout5, 400, 300);
-
 		
 		
-		Button switchToMenuFromAddBookButton = new Button("Menu");
-		switchToMenuFromAddBookButton.setOnAction(e -> primaryStage.setScene(scene1));
-
-		Button twoswitchToScene1Button = new Button("Menu");
-		twoswitchToScene1Button.setOnAction(e -> primaryStage.setScene(scene1));
-
-		Button threeswitchToScene1Button = new Button("Menu");
-		threeswitchToScene1Button.setOnAction(e -> primaryStage.setScene(scene1));
-
-		Button fourswitchToScene1Button = new Button("Menu");
-		fourswitchToScene1Button.setOnAction(e -> primaryStage.setScene(scene1));
-
+		/* ADD BOOK SECTION */
 		
 		
-	
-		
-		BorderPane addNewBookLayout = new BorderPane();
 		HBox hBox = new HBox();
 		hBox.setSpacing(10);
-		Button addNewBook = new Button("Add Book");
-		addNewBook.setOnAction(e -> addNewBook(bookCatalog));
-		
-		hBox.getChildren().add(switchToMenuFromAddBookButton);
-		hBox.getChildren().add(addNewBook);
-		addNewBookLayout.setBottom(hBox);
 		hBox.setPadding(new Insets(0, 0, 20, 200));
 
-		Scene addNewBookscene = new Scene(addNewBookLayout, 400, 300);
+		BorderPane addNewBookLayout = new BorderPane();
+		addNewBookLayout.setBottom(hBox);
+		Scene addNewBookScene = new Scene(addNewBookLayout, 400, 300);
 		
+		//Menu button to switch to add new book scene
+		Button addButton = new Button(" Add a new book ");
+		menuLayout.getChildren().add(addButton);
+		addButton.setOnAction(e -> primaryStage.setScene(addNewBookScene));
+
+		//Switch back menu button
+		Button switchToMenuFromAddBookButton = new Button("Menu");
+		switchToMenuFromAddBookButton.setOnAction(e -> primaryStage.setScene(scene1));
+		hBox.getChildren().add(switchToMenuFromAddBookButton);
 		
+		//Button to add a new book to the catalog
+		Button addNewBook = new Button("Add Book");
+		addNewBook.setOnAction(e -> addNewBook(bookCatalog));
+		hBox.getChildren().add(addNewBook);
+
+		//Grid layout for text fields
 		GridPane gridPane = new GridPane();
 		gridPane.setHgap(10);
 		gridPane.setVgap(10);
-		
+
+		//Labels and text fields
 		Label bookTitleLabel = new Label("Title");
 		Label authorLabel = new Label("Author");
-		
+
 		bookTitleTf = new TextField();
 		authorTf = new TextField();
-		
+
+		//Adds labels and fields to grid
 		gridPane.add(bookTitleLabel, 0, 0);
 		gridPane.add(authorLabel, 0, 1);
 		gridPane.add(bookTitleTf, 1, 0);
 		gridPane.add(authorTf, 1, 1);
-		
+
+		//Adds grid to layout
 		addNewBookLayout.setCenter(gridPane);
 		gridPane.setPadding(new Insets(100, 0, 0, 90));
-		
-		
-		
-		Button addButton = new Button(" Add a new book ");
-		addButton.setOnAction(e -> primaryStage.setScene(addNewBookscene));
-		layout1.getChildren().add(addButton);
 
+		
+		
+		/* RENT BOOK SECTION */
+		
+		
+		VBox rentBooklayout = new VBox();
+		Scene rentBookScene = new Scene(rentBooklayout, 400, 300);
+		
+		//Menu button to switch to rent book scene 
 		Button rentButton = new Button("     Rent a book    ");
-		rentButton.setOnAction(e -> primaryStage.setScene(scene3));
-		layout1.getChildren().add(rentButton);
+		rentButton.setOnAction(e -> primaryStage.setScene(rentBookScene));
+		menuLayout.getChildren().add(rentButton);
+		
+		//Switch back menu button
+		Button switchToMenuFromRentBookButton = new Button("Menu");
+		switchToMenuFromRentBookButton.setOnAction(e -> primaryStage.setScene(scene1));
+		rentBooklayout.getChildren().add(switchToMenuFromRentBookButton);
 
+
+		
+		/* RETURN BOOK SECTION */
+	
+		
+		VBox returnBookLayout = new VBox();
+		Scene returnBookScene = new Scene(returnBookLayout, 400, 300);
+		
+		//Menu button to switch to return book scene
 		Button returnButton = new Button("   Return a book   ");
-		returnButton.setOnAction(e -> primaryStage.setScene(scene4));
-		layout1.getChildren().add(returnButton);
+		returnButton.setOnAction(e -> primaryStage.setScene(returnBookScene));
+		menuLayout.getChildren().add(returnButton);
 
+		//Switch back menu button
+		Button switchToMenuFromReturnBookButton = new Button("Menu");
+		switchToMenuFromReturnBookButton.setOnAction(e -> primaryStage.setScene(scene1));
+		returnBookLayout.getChildren().add(switchToMenuFromReturnBookButton);
+
+		
+		
+		/* LOOKUP BOOK SECTION */
+		
+		
+		VBox lookupBook = new VBox();
+		Scene lookupBookScene = new Scene(lookupBook, 400, 300);
+		
+		//Menu button to switch to lookup scene
 		Button searchButton = new Button("  Look up a book  ");
-		searchButton.setOnAction(e -> primaryStage.setScene(scene5));
-		layout1.getChildren().add(searchButton);
-
+		searchButton.setOnAction(e -> primaryStage.setScene(lookupBookScene));
+		menuLayout.getChildren().add(searchButton);
 		
+		//Switch back menu button
+		Button switchToMenuFromLookupBookButton = new Button("Menu");
+		switchToMenuFromLookupBookButton.setOnAction(e -> primaryStage.setScene(scene1));
+		lookupBook.getChildren().add(switchToMenuFromLookupBookButton);
 		
 
-		layout3.getChildren().add(twoswitchToScene1Button);
-		layout4.getChildren().add(threeswitchToScene1Button);
-		layout5.getChildren().add(fourswitchToScene1Button);
 
-		
+		/* PRIMARY STAGE */
 		
 		
 		primaryStage.setScene(scene1);
@@ -128,41 +148,38 @@ public class Main extends Application {
 		primaryStage.setTitle("Scene Toggling Example");
 		primaryStage.show();
 	}
-	
-	
-	public ArrayList<Book> addNewBook(ArrayList<Book> bookCatalog)
-	{
-		String bookTitle = bookTitleTf.getText();
-		String author =  authorTf.getText();
-		
-		Book newBook = new Book(bookTitle, author);
-		
-		
 
-		int numOfBooks = bookCatalog.size();;
-		
 	
+	
+	
+	
+	public ArrayList<Book> addNewBook(ArrayList<Book> bookCatalog) {
+		String bookTitle = bookTitleTf.getText();
+		String author = authorTf.getText();
+
+		Book newBook = new Book(bookTitle, author);
+
+		int numOfBooks = bookCatalog.size();
+		;
 
 		// Sets the id of the book
 		newBook.id = numOfBooks + 1;
 
-		
-
 		bookCatalog.add(newBook);
 
-		for(int x = 0; x < numOfBooks; x++)
-		{
+		for (int x = 0; x < numOfBooks; x++) {
 			System.out.println(bookCatalog.get(x).title);
 		}
 
 		return bookCatalog;
 	}
+
+	
 	
 	
 	public static void main(String[] args) {
 		launch(args);
-		
+
 	}
-	
 
 }
