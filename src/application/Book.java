5a94ed1000ce6483d2fc08df9606da71912ee542
Zachary Leyes <leyeszach@gmail.com>
2023-08-01 17:38:57 -0400

    added more comments for code clarity

--- src/application/Book.java
+++ src/application/Book.java
@@ -11,7 +11,7 @@ public class Book {
 	private Date lentDate;
 	private int id;
 
-	// Constructor for adding a new book
+	//Constructor for a book object
 	Book(String title, String author) {
 		this.title = title;
 		this.author = author;
@@ -19,6 +19,7 @@ public class Book {
 		this.lentDate = null;
 	}
 	
+	//Constructor for a book object
 	Book(int bookId, String title, String author, String status) {
 		this.id = bookId;
 		this.title = title;
@@ -26,47 +27,56 @@ public class Book {
 		this.status = status;
 	}
 
+	//Getter for date
 	public Date getDate() {
 		return lentDate;
 	}
 	
+	//Sets a book's date to null
 	public void setDateNull()
 	{
 		this.lentDate = null;
 	}
 
-	
+	//Setter for date
 	public void setDate(Date lentDate)
 	{
 		this.lentDate = lentDate;
 	}
 	
+	//Getter for title
 	public String getTitle() {
 		return title;
 	}
 
+	//Getter for author
 	public String getAuthor() {
 		return author;
 	}
 
+	//Getter for status
 	public String getStatus() {
 		return status;
 	}
 
+	//Getter for id
 	public int getId() {
 		return id;
 	}
 
+	//Rents a book
 	public void rentBook() {
-		this.status = "unavailable";
-		this.lentDate = new Date();
+		this.status = "unavailable";//sets book to unavailable
+		this.lentDate = new Date();//sets lent date to the current time
 	}
 
+	//Returns a book
 	public void returnBook() {
-		this.status = "available";
-		this.lentDate = null;
+		this.status = "available";//sets book to available
+		this.lentDate = null;//sets lent date to null
 	}
 
+	//Sets a books id
 	public void setId(ArrayList<Book> bookCatalog) {
 		int numOfBooks = bookCatalog.size();
 		int id = numOfBooks + 1;
--- src/application/Main.java
+++ src/application/Main.java
@@ -17,11 +17,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Scanner;
-import java.io.PrintWriter;
 import java.io.FileWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -46,60 +43,48 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		
+
+		//Assigns primary stage to a variable 
 		this.primaryStage = primaryStage;
 
+		//Book catalog 
 		ArrayList<Book> bookCatalog = new ArrayList<>();
 
+		//Name for file
 		String fileName = "BookCatalog.txt";
 
+		//Reads BookCatalog.txt file and stores values into books objects that are then placed in the book catalog array list
 		try {
 			File file = new File(fileName);
 			Scanner scanner = new Scanner(file);
-		
 
 			while (scanner.hasNextLine()) {
-				int bookId = Integer.parseInt(scanner.nextLine());
+				int bookId = Integer.parseInt(scanner.nextLine());//Parses the string into an integer value
 				String bookTitle = scanner.nextLine();
 				String bookAuthor = scanner.nextLine();
 				String bookStatus = scanner.nextLine();
 				String date = scanner.nextLine();
-				
-				System.out.println(bookId);
-				System.out.println(bookTitle);
-				System.out.println(bookAuthor);
-				System.out.println(bookStatus);
 
+				//Makes a new book object 
 				Book newBook = new Book(bookId, bookTitle, bookAuthor, bookStatus);
 
 				if (date.equals("null")) {
-					System.out.println("null");
-					newBook.setDateNull();
+					newBook.setDateNull();//sets the book's date to null if the string "null" is read in 
 				} else {
-					String pattern = "EEE MMM dd HH:mm:ss zzz yyyy";
-					SimpleDateFormat sdf = new SimpleDateFormat(pattern);
-					Date lendDate = sdf.parse(date);
-					System.out.println(date);
-					newBook.setDate(lendDate);
-					
+					String pattern = "EEE MMM dd HH:mm:ss zzz yyyy"; //defines the pattern for the date formatter 
+					SimpleDateFormat sdf = new SimpleDateFormat(pattern);// Date formatter to parse an string into a date object
+					Date lendDate = sdf.parse(date); //converts string into a date object
+					newBook.setDate(lendDate);// sets a book's date
 				}
-
-				bookCatalog.add(newBook);
-			
-				
+				bookCatalog.add(newBook);// adds book to the catalog
 			}
-		
-
 			scanner.close();
-
 		} catch (FileNotFoundException e) {
-			System.out.println("The file does not exist or cannot be read: " + e.getMessage());
+			System.out.println("The file does not exist");
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-			
-			
-			
+
 		
 		/* MENU SECTION */
 
@@ -126,11 +111,11 @@ public class Main extends Application {
 		//Menu button to switch to add new book scene
 		Button addButton = new Button(" Add a new book ");
 		menuLayout.getChildren().add(addButton);
-		addButton.setOnAction(e -> primaryStage.setScene(addNewBookScene));
+		addButton.setOnAction(e -> primaryStage.setScene(addNewBookScene));//Sets scene to the add new book scene
 
 		//Switch back to menu button
 		Button switchToMenuFromAddBookButton = new Button("Menu");
-		switchToMenuFromAddBookButton.setOnAction(e -> primaryStage.setScene(mainMenuScene));
+		switchToMenuFromAddBookButton.setOnAction(e -> primaryStage.setScene(mainMenuScene));//Sets scene back to the main menu
 		addBookHBox.getChildren().add(switchToMenuFromAddBookButton);
 		
 		//Button to add a new book to the catalog
@@ -175,27 +160,29 @@ public class Main extends Application {
 		
 		//Menu button to switch to rent book scene 
 		Button rentButton = new Button("     Rent a book    ");
-		rentButton.setOnAction(e -> updateRentTable(bookCatalog));
+		rentButton.setOnAction(e -> updateRentTable(bookCatalog));//sets scene to the rent book table and updates the table with the correct available books
 		menuLayout.getChildren().add(rentButton);
 		
 		//Switch back to menu button
 		Button switchToMenuFromRentBookButton = new Button("Menu");
-		switchToMenuFromRentBookButton.setOnAction(e -> primaryStage.setScene(mainMenuScene));
+		switchToMenuFromRentBookButton.setOnAction(e -> primaryStage.setScene(mainMenuScene));//sets scene back to the main menu
 		rentBookHBox.getChildren().add(switchToMenuFromRentBookButton);
 		
 		//Button to rent a book
 		Button rentBookButton = new Button("Rent Book");
-		rentBookButton.setOnAction(e -> rentBook(bookCatalog, fileName));
+		rentBookButton.setOnAction(e -> rentBook(bookCatalog, fileName)); //rents a book and rewrites BookCatalog.txt file  
 		rentBookHBox.getChildren().add(rentBookButton);
 		
 		//Table view for available books
 		availableBooksTableView = new TableView();
 		rentBookBorderPane.setCenter(availableBooksTableView);
 		
+		//Resizes columns based of table space gives each column an equal percentage of that space
 		availableBooksTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 		
+		//Adds table Columns and sets table cell values 
 		TableColumn bookIdColumn = new TableColumn<Book, String>("Id");
-		bookIdColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("id"));
+		bookIdColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("id"));// Pulls ids from book objects by calling the getId() getter method (works by capitalizing the first letter in the string and then places get before said string then calls that method)
 		
 		TableColumn bookTitleColumn = new TableColumn<Book, String>("Title");
 		bookTitleColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("title"));
@@ -204,11 +191,12 @@ public class Main extends Application {
 		bookAuthorColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("author"));
 		
 		
-		
+		//Adds columns to the table
 		availableBooksTableView.getColumns().add(bookIdColumn);
 		availableBooksTableView.getColumns().add(bookTitleColumn);
 		availableBooksTableView.getColumns().add(bookAuthorColumn);
 		
+		//Adds a custom message if there is no data in the table (no books available)
 	    Label customMessageForRentTable = new Label("All books are currently being rented");
         availableBooksTableView.setPlaceholder(customMessageForRentTable);
 
@@ -240,11 +228,14 @@ public class Main extends Application {
 		returnBookButton.setOnAction(e -> returnBook(bookCatalog, fileName));
 		returnBookHBox.getChildren().add(returnBookButton);
 		
+		//Table view for unavailable books
 		unavailableBooksTableView = new TableView();
 		returnBookBorderPane.setCenter(unavailableBooksTableView);
 		
+		//Sets column size
 		unavailableBooksTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 		
+		//Table Columns
 		TableColumn rentedBookIdColumn = new TableColumn<Book, String>("Id");
 		rentedBookIdColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("id"));
 		
@@ -258,13 +249,13 @@ public class Main extends Application {
 		lentDateColumn.setCellValueFactory(new PropertyValueFactory<Book, Date>("date"));
 		
 		
-		
+		//Adds columns to bale
 		unavailableBooksTableView.getColumns().add(rentedBookIdColumn);
 		unavailableBooksTableView.getColumns().add(rentedBookTitleColumn);
 		unavailableBooksTableView.getColumns().add(rentedBookAuthorColumn);
 		unavailableBooksTableView.getColumns().add(lentDateColumn);
 
-		  
+		//Adds custom message for table
         Label customMessageForReturnTable = new Label("All books have been returned");
         unavailableBooksTableView.setPlaceholder(customMessageForReturnTable);
 		
@@ -300,12 +291,15 @@ public class Main extends Application {
 		HBox searchBarHBox = new HBox();
 		searchBarHBox.setSpacing(10);
 		
+		//Search bar label and text field
 		Label searchBarLabel = new Label("Search Bar");
 		searchBarTf = new TextField();
 
+		//Adds search box to the horizontal box layout
 		searchBarHBox.getChildren().add(searchBarLabel);
 		searchBarHBox.getChildren().add(searchBarTf);
 		
+		//Places the search bar layout within the border pane
 		searchBorderPane.setTop(searchBarHBox);
 		searchBarHBox.setPadding(new Insets(10, 0, 0, 80));
 		
@@ -313,8 +307,10 @@ public class Main extends Application {
 		foundTableView = new TableView();
 		searchBorderPane.setCenter(foundTableView);
 		
+		//Resizes columns
 		foundTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 		
+		//Table columns
 		TableColumn foundBookIdColumn = new TableColumn<Book, String>("Id");
 		foundBookIdColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("id"));
 		
@@ -328,13 +324,13 @@ public class Main extends Application {
 		foundStatusColumn.setCellValueFactory(new PropertyValueFactory<Book, Date>("status"));
 		
 		
-		
+		//Adds columns to the table
 		foundTableView.getColumns().add(foundBookIdColumn);
 		foundTableView.getColumns().add(foundBookTitleColumn);
 		foundTableView.getColumns().add(foundBookAuthorColumn);
 		foundTableView.getColumns().add(foundStatusColumn);
 
-		  
+		//Adds custom message 
         Label customMessageForSearchTable = new Label("No matches found");
         foundTableView.setPlaceholder(customMessageForSearchTable);
 		
@@ -348,31 +344,40 @@ public class Main extends Application {
 		primaryStage.show();
 	}
 	
+	
+	
+	//Switches scene to rent a book scene and updates the available books table 
 	public void updateRentTable(ArrayList<Book> bookCatalog) {
-	    // Update the TableView data
+		
+	    //Clears data from table 
 	    availableBooksTableView.getItems().clear();
 
 	    int numOfBooks = bookCatalog.size();
 
+	    //Adds all available book's data to the table 
 	    for (int x = 0; x < numOfBooks; x++) {
 	        if (bookCatalog.get(x).getStatus().equals("available")) {
-	            availableBooksTableView.getItems().add(bookCatalog.get(x));
+	            availableBooksTableView.getItems().add(bookCatalog.get(x));//adds available book objects to the table 
 	        }
 	    }
-
+	    
 	    // Switches to rent book scene
 	    primaryStage.setScene(rentBookScene);
 	}
 
+	
+	//Switches scene to return a book scene and updates the unavailable books table 
 	public void updateReturnTable(ArrayList<Book> bookCatalog) {
-	    // Update the TableView data
+		
+	    //Clears data from table
 	    unavailableBooksTableView.getItems().clear();
 
 	    int numOfBooks = bookCatalog.size();
 
+	    //Adds all unavailable book's data to the table 
 	    for (int x = 0; x < numOfBooks; x++) {
 	        if (bookCatalog.get(x).getStatus().equals("unavailable")) {
-	            unavailableBooksTableView.getItems().add(bookCatalog.get(x));
+	            unavailableBooksTableView.getItems().add(bookCatalog.get(x));//adds unavailable book objects to the table 
 	        }
 	    }
 
@@ -380,55 +385,69 @@ public class Main extends Application {
 	    primaryStage.setScene(returnBookScene);
 	}
 
+	
+	//Adds a new book to book catalog and writes the new book to the BookCatalog.txt file
 	public ArrayList<Book> addNewBook(ArrayList<Book> bookCatalog,String fileName) {
+		
+		//Stores values in text fields into variables
 		String bookTitle = bookTitleTf.getText();
 		String author = authorTf.getText();
 
-		if (bookTitle == "" || author == "") {
+		//Exits if the user enters an empty value for the author or title
+		if (bookTitle.equals("") || author.equals("")) {
 			return bookCatalog;
 		}
 
+		//Creates a new book object
 		Book newBook = new Book(bookTitle, author);
 		
-		// Sets the id of the book
+		//Sets the id of the book
 		newBook.setId(bookCatalog);
 
+		//Appends new book to the file
 		try {
 		
-
-			FileWriter fileWriter = new FileWriter(fileName, true);
-
-            PrintWriter output = new PrintWriter(fileWriter);
+			//Writes to the file in append mode
+			FileWriter fileWriter = new FileWriter(fileName, true); 
+            PrintWriter output = new PrintWriter(fileWriter); 
             
+            //Writes each book's member variable to the file
             output.println(newBook.getId());
             output.println(newBook.getTitle());
             output.println(newBook.getAuthor());
             output.println(newBook.getStatus());
             output.println("null");
+            
             output.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		   
-
+		//Adds book to the book catalog
 		bookCatalog.add(newBook);
 
+		//Clears the text fields
 		bookTitleTf.clear();
 		authorTf.clear();
 
 		return bookCatalog;
 	}
 
+	
+	//Rents a book and rewrites the BookCatalog.txt file so that the file contains the new book status and lent date
 	public ArrayList<Book> rentBook(ArrayList<Book> bookCatalog, String fileName) {
 		
 		try {
-			Book selectedBook = availableBooksTableView.getSelectionModel().getSelectedItem();
-			int row = availableBooksTableView.getSelectionModel().getSelectedIndex();
+			Book selectedBook = availableBooksTableView.getSelectionModel().getSelectedItem(); //Gets the book object selected by the user
+			int row = availableBooksTableView.getSelectionModel().getSelectedIndex();//Gets the row number selected by the user 
 
+			//Rents the selected book
 			selectedBook.rentBook();
 
+			//Rewrites the file
 			updateFile(bookCatalog, fileName);
 
+			//Deletes the row in table 
 			deleteRow(row, availableBooksTableView);
 
 		} catch (Exception e) {
@@ -438,32 +457,44 @@ public class Main extends Application {
 		return bookCatalog;
 	}
 
+	
+	//Returns a book and rewrites the file
 	public ArrayList<Book> returnBook(ArrayList<Book> bookCatalog, String fileName) {
+		
 		try {
-			Book selectedBook = unavailableBooksTableView.getSelectionModel().getSelectedItem();
-			int row = unavailableBooksTableView.getSelectionModel().getSelectedIndex();
+			Book selectedBook = unavailableBooksTableView.getSelectionModel().getSelectedItem();//Gets the book object selected by the user
+			int row = unavailableBooksTableView.getSelectionModel().getSelectedIndex();//Gets the row number selected by the user
 
+			//Returns selected book
 			selectedBook.returnBook();
 			
+			//Rewrites the file
 			updateFile(bookCatalog, fileName);
 
+			//Deletes the row in table 
 			deleteRow(row, unavailableBooksTableView);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return bookCatalog;
 	}
-
+	
+	//Searches for a book then displays that book in the table view
 	public void searchBook(ArrayList<Book> bookCatalog) {
+		
 		try {
-			String searchValue = searchBarTf.getText();
+			String searchValue = searchBarTf.getText();//Stores user's search value 
 			
+			//Clears table data
 			foundTableView.getItems().clear();
 
 			int numOfBooks = bookCatalog.size();
 
+			//Searches through each book in the catalog
 			for (int x = 0; x < numOfBooks; x++) {
+				//If the book's author or title contains the user's search value adds that book object to the table
 				if (bookCatalog.get(x).getTitle().contains(searchValue) || bookCatalog.get(x).getAuthor().contains(searchValue)) {
 					foundTableView.getItems().add(bookCatalog.get(x));
 				}
@@ -475,34 +506,38 @@ public class Main extends Application {
 	}
 	
 	
+	//Rewrites the file with the updated book catalog
 	public void updateFile(ArrayList<Book> bookCatalog, String fileName) {
 
 		File file = new File(fileName);
 
-		PrintWriter output;
+		
 		try {
-			output = new PrintWriter(file);
+			
+			//Rewrites file
+			PrintWriter output = new PrintWriter(file);
 
 			int numOfBooks = bookCatalog.size();
 
+			//Writes all books in the book catalog to the file
 			for (int x = 0; x < numOfBooks; x++) {
 				output.println(bookCatalog.get(x).getId());
 				output.println(bookCatalog.get(x).getTitle());
 				output.println(bookCatalog.get(x).getAuthor());
 				output.println(bookCatalog.get(x).getStatus());
 
-
+				//Gets a books date
 				Date date = bookCatalog.get(x).getDate();
 
+				//Writes a book's lent date using the date formatter to the file or writes null if a book's lent date is null
 				if (date != null) {
-					String pattern = "EEE MMM dd HH:mm:ss zzz yyyy";
-					SimpleDateFormat sdf = new SimpleDateFormat(pattern);
-					String lendDateString = sdf.format(date);
-					output.println(lendDateString);
+					String pattern = "EEE MMM dd HH:mm:ss zzz yyyy";//pattern for the date formatter
+					SimpleDateFormat sdf = new SimpleDateFormat(pattern);//date formatter to change a date object to a string value 
+					String lendDateString = sdf.format(date);//converts date object to a string value
+					output.println(lendDateString);//writes date string value to file
 				} else {
 					output.println("null");
 				}
-				
 			}
 			output.close();
 		} catch (FileNotFoundException e) {
@@ -511,9 +546,11 @@ public class Main extends Application {
 
 	}
 
+	//Deletes a row in a table view
 	void deleteRow(int row, TableView<Book> tableView) {
+		//Checks if a row was selected
 		if (row >= 0) {
-			tableView.getItems().remove(row);
+			tableView.getItems().remove(row);//deletes selected row from the table view
 		}
 	}
 
